1cbada5069006d3ce713dd78564ac8a808475d3b
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-20 11:41:35 -0300

    commit

---
+++ src/main/java/org/example/sudoku/Coordinates.java
@@ -0,0 +1,24 @@
+package org.example.sudoku;
+
+public class Coordinates {
+
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this === 0) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+}
